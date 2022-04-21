13e9b3bbd8d76f469f1ee1d22eaf8d1b749583d2
RalphBrynard <ralph@thebrynards.com>
2022-04-20 20:08:22 -0400

    Added logic for psscriptroot execution

--- scripts/setup.ps1
+++ scripts/setup.ps1
@@ -1,10 +1,10 @@
 Write-Host "  - Copying files ... "
-$sourcePath = "..\documents\*"
+$sourcePath = "$PSScriptRoot\documents\*"
 $destPath = "C:\Users\Sophos\Documents\"
 Copy-Item -Force -Recurse -Verbose $sourcePath -Destination $destPath
 
 Write-Host " - Installing Python Requirements ... "
-Start-Process -FilePath py.exe -ArgumentList "-m pip install -r ..\python\requirements.txt"
+Start-Process -FilePath py.exe -ArgumentList "-m pip install -r $PSScriptRoot\python\requirements.txt"
 
 Write-Host " - Starting Office 2016 Installation ... "
 Write-Host " - Mounting ISO Image ... "
@@ -13,8 +13,8 @@ $imgPath = "C:\office_2016_en_US.iso"
 Mount-DiskImage -ImagePath $imgPath
 
 Write-Host " - Starting Office Install ... "
-Start-Process -NoNewWindow -FilePath "D:\setup.exe" -ArgumentList "/configure ..\assets\O365Install.xml /quiet /passive"
+Start-Process -NoNewWindow -FilePath "D:\setup.exe" -ArgumentList "/configure $PSScriptRoot\assets\O365Install.xml /quiet /passive"
 
 Write-Host " - Running Office Python Setup Script ... "
-Start-Process -FilePath py.exe -ArgumentList "..\python\setupOutlook.py"
+Start-Process -FilePath py.exe -ArgumentList "$PSScriptRoot\python\setupOutlook.py"
 
